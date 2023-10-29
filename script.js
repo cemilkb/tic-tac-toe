5ec3943f5834b1a3b3be7c758395b90264b40da7
cemilkb <cemilkoybasi@hotmail.com>
2023-10-29 16:14:44 +0300

    tasarım tamamlandı sayfaya efektivite eklenicek

--- script.js
+++ script.js
@@ -5,47 +5,59 @@ let oPlayerButton = document.getElementById("o-p-button")
 let oAiButton = document.getElementById("o-ai-button")
 let playerX
 let playerO
+let isChooseX = false
+let isChooseO = false
 
 xPlayerButton.addEventListener("click", () => {
     xAiButton.classList.remove("text-[#190482]", "bg-x/[.6]", "scale-105")
     xPlayerButton.classList.add("text-[#190482]", "bg-x/[.6]", "scale-105")
     playerX = "player"
+    isChooseX = true
 })
 
 xAiButton.addEventListener("click", () => {
     xPlayerButton.classList.remove("text-[#190482]", "bg-x/[.6]", "scale-105")
     xAiButton.classList.add("text-[#190482]", "bg-x/[.6]", "scale-105")
     playerX = "ai"
+    isChooseX = true
 })
 
 oPlayerButton.addEventListener("click", () => {
     oAiButton.classList.remove("text-[#190482]", "bg-o/[.6]", "scale-105")
     oPlayerButton.classList.add("text-[#190482]", "bg-o/[.6]", "scale-105")
     playerO = "player"
+    isChooseO = true
 })
 
 oAiButton.addEventListener("click", () => {
     oPlayerButton.classList.remove("text-[#190482]", "bg-o/[.6]", "scale-105")
     oAiButton.classList.add("text-[#190482]", "bg-o/[.6]", "scale-105")
     playerO = "ai"
+    isChooseO = true
 })
 
 // Start Game
 
 const startPage = document.getElementById("start-page")
 const gameStartButton = document.getElementById("start-button")
-const name1 = document.getElementById("name-1")
-const name2 = document.getElementById("name-2")
+const xNameInput = document.getElementById("x-name-input")
+const oNameInput = document.getElementById("o-name-input")
+const xName = document.getElementById("x-name")
+const oName = document.getElementById("o-name")
 
 gameStartButton.addEventListener("click", () => {
-    startPage.classList.add("opacity-0")
-    startPage.classList.add("scale-0")
-    console.log(name1.value)
-    console.log(name2.value)
-    setTimeout(()=>{startPage.classList.add("hidden")
-    gamePage.classList.remove("hidden")
-    gamePage.classList.remove("opacity-0")
-    gamePage.classList.remove("scale-0")},500)
+    if (isChooseO == true && isChooseX == true) {
+        startPage.classList.add("opacity-0")
+        startPage.classList.add("scale-0")
+        setTimeout(() => {
+            startPage.classList.add("hidden")
+            gamePage.classList.remove("hidden")
+            gamePage.classList.remove("opacity-0")
+            gamePage.classList.remove("scale-0")
+        }, 500)
+        xName.textContent = xNameInput.value
+        oName.textContent = oNameInput.value
+    } else { alert("NAAPTIN SEN YEĞEN YAW") }
 })
 
 // Back To Home
@@ -60,5 +72,20 @@ homeButton.addEventListener("click", () => {
         startPage.classList.remove("hidden")
         startPage.classList.remove("opacity-0")
         startPage.classList.remove("scale-0")
-    },500)
+    }, 500)
+})
+
+let deneme = document.querySelectorAll(".area")
+
+deneme.forEach((e)=>{
+    e.addEventListener("mouseover", ()=>{
+        e.classList.add("bg-[red]")
+    })
+    e.addEventListener("mouseout", ()=>{
+        e.classList.remove("bg-[red]")
+    })
+    e.addEventListener("click", ()=>{
+        e.innerHTML = `<span class="text-[70px] text-x font-blackops">X</span>`
+        console.log(e.id)
+    })
 })
