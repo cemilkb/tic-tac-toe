af79ca947e82a4e42e4203851e7014523a04cb43
cemilkb <cemilkoybasi@hotmail.com>
2024-02-20 12:40:06 +0300

    ai done

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let game = {
     oName: prompt("Name ="),
     oIsHuman: prompt("Player O Human or Ai ?"),
     oXo: "O",
-    gameBoard: {
-        a: ["", "", ""],
-        b: ["", "", ""],
-        c: ["", "", ""]
-    },
+    gameBoard: [
+        "", "", "",
+        "", "", "",
+        "", "", ""
+    ],
     turn: {
         count: 0,
         addTurn: function () {
@@ -17,15 +17,16 @@ let game = {
         }
     },
     humanPlay: function (wich) {
-        let get = prompt("harf")
-        let set = get.split("-")
-        let value = wich
-        let arrayFill = (() => {
-            game.gameBoard[set[0]][set[1]] = value
-        })()
+        let get = prompt(`0-8 arası bir sayı giriniz`)
+        game.gameBoard[get] = wich
+        game.aiCanChoose.splice(choose, 1)
     },
+    aiCanChoose: [0, 1, 2, 3, 4, 5, 6, 7, 8],
     aiPlay: function (wich) {
-        alert(wich)
+        let choose = Math.floor(Math.random() * game.aiCanChoose.length)
+        let get = game.aiCanChoose[choose]
+        game.gameBoard[get] = wich
+        game.aiCanChoose.splice(choose, 1)
     },
     makePlayer: function (name, isHuman, xo) {
         let humanPlay = this.humanPlay
@@ -64,9 +65,9 @@ let game = {
     },
     play: function (wich) {
         alert(`           1  2  3
-       a- [${this.gameBoard["a"][0]}] [${this.gameBoard["a"][1]}] [${this.gameBoard["a"][2]}]
-       b- [${this.gameBoard["b"][0]}] [${this.gameBoard["b"][1]}] [${this.gameBoard["b"][2]}]
-       c  [${this.gameBoard["c"][0]}] [${this.gameBoard["c"][1]}] [${this.gameBoard["c"][2]}]`)
+       a- [${this.gameBoard[0]}] [${this.gameBoard[1]}] [${this.gameBoard[2]}]
+       b- [${this.gameBoard[3]}] [${this.gameBoard[4]}] [${this.gameBoard[5]}]
+       c  [${this.gameBoard[6]}] [${this.gameBoard[7]}] [${this.gameBoard[8]}]`)
         if (wich.isHuman == "Human") {
             this.turn.addTurn()
             this.humanPlay(wich.xo)
@@ -77,14 +78,14 @@ let game = {
 
     },
     isWin: function (wich) {
-        if (this.gameBoard["a"][0] == wich && this.gameBoard["a"][1] == wich && this.gameBoard["a"][2] == wich ||
-            this.gameBoard["b"][0] == wich && this.gameBoard["b"][1] == wich && this.gameBoard["b"][2] == wich ||
-            this.gameBoard["c"][0] == wich && this.gameBoard["c"][1] == wich && this.gameBoard["c"][2] == wich ||
-            this.gameBoard["a"][0] == wich && this.gameBoard["b"][0] == wich && this.gameBoard["c"][0] == wich ||
-            this.gameBoard["a"][1] == wich && this.gameBoard["b"][1] == wich && this.gameBoard["c"][1] == wich ||
-            this.gameBoard["a"][2] == wich && this.gameBoard["b"][2] == wich && this.gameBoard["c"][2] == wich ||
-            this.gameBoard["a"][0] == wich && this.gameBoard["b"][1] == wich && this.gameBoard["c"][2] == wich ||
-            this.gameBoard["a"][2] == wich && this.gameBoard["b"][1] == wich && this.gameBoard["c"][0] == wich) {
+        if (this.gameBoard[0] == wich && this.gameBoard[1] == wich && this.gameBoard[2] == wich ||
+            this.gameBoard[3] == wich && this.gameBoard[4] == wich && this.gameBoard[5] == wich ||
+            this.gameBoard[6] == wich && this.gameBoard[7] == wich && this.gameBoard[8] == wich ||
+            this.gameBoard[0] == wich && this.gameBoard[3] == wich && this.gameBoard[6] == wich ||
+            this.gameBoard[1] == wich && this.gameBoard[4] == wich && this.gameBoard[7] == wich ||
+            this.gameBoard[2] == wich && this.gameBoard[5] == wich && this.gameBoard[8] == wich ||
+            this.gameBoard[0] == wich && this.gameBoard[4] == wich && this.gameBoard[8] == wich ||
+            this.gameBoard[2] == wich && this.gameBoard[4] == wich && this.gameBoard[6] == wich) {
             return true
         }
     },
@@ -112,11 +113,12 @@ let game = {
         }
     },
     stop: function (wich) {
-        this.gameBoard = {
-            a: ["", "", ""],
-            b: ["", "", ""],
-            c: ["", "", ""]
-        }
+        this.gameBoard = [
+            "", "", "",
+            "", "", "",
+            "", "", ""
+        ]
+        this.aiCanChoose = [0, 1, 2, 3, 4, 5, 7, 8]
         this.turn.count = 0
         if (wich == "draw") {
             alert("BARABARA")
@@ -141,10 +143,3 @@ console.log(playerO)
 
 //game.moveX()
 
-let gameBoard = [
-    "", "", "",
-    "", "a", "",
-    "", "", ""
-]
-
-console.log(gameBoard.indexOf("a"))
